1878bc2e3b0e5c6e9eadaf749cb02760af2e48e1
Thiago-Chiloff <135349098+Thiago-Chiloff@users.noreply.github.com>
2024-04-05 10:51:14 -0300

    Update calculadora de raiz quadrada.js

--- calculadora de raiz quadrada.js
+++ calculadora de raiz quadrada.js
@@ -6,8 +6,8 @@ const resposta = document.querySelector('h3');
 corpo.addEventListener  ('click' , (e) => {
     e.preventDefault();
 
-    const nuemro = Number(corpo.querySelector('.numero').value);
-    const raiz = Math.sqrt(nuemro);
+    const numero = Number(corpo.querySelector('.numero').value);
+    const raiz = Math.sqrt(numero);
     
     resposta.innerText = 'Seu resultado é: '  + raiz;
 
